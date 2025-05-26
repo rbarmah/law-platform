1405600559e1bda43de2407af28d3019228d9ebe
Ruthie Armah <104768366+rbarmah@users.noreply.github.com>
2025-05-26 09:29:44 -0700

    Add ring color configuration to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,11 @@ export default {
         'hubtel-orange': '#FF6634',
         'hubtel-teal': '#00C9B6',
         'hubtel-navy': '#001B36'
+      },
+      ringColor: {
+        'hubtel-orange': '#FF6634',
+        'hubtel-teal': '#00C9B6',
+        'hubtel-navy': '#001B36'
       }
     }
   },
