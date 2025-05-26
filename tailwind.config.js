391f6595ee83dad9c2ff0ed357509011b4a475d7
Ruthie Armah <104768366+rbarmah@users.noreply.github.com>
2025-05-26 09:23:00 -0700

    Update color scheme to match Hubtel brand

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,32 @@ export default {
   darkMode: 'class',
   theme: {
     extend: {
+      colors: {
+        'hubtel-orange': '#FF6634',
+        'hubtel-teal': '#00C8C8',
+        'hubtel-navy': '#001B36',
+        'navy': {
+          50: '#F0F4F8',
+          100: '#D9E2EC',
+          200: '#BCCCDC',
+          300: '#9FB3C8',
+          400: '#829AB1',
+          500: '#486581',
+          600: '#334E68',
+          700: '#243B53',
+          800: '#102A43',
+          900: '#001B36',
+        }
+      },
       typography: {
         DEFAULT: {
           css: {
             maxWidth: 'none',
             color: 'inherit',
             a: {
-              color: '#3b82f6',
+              color: '#00C8C8',
               '&:hover': {
-                color: '#2563eb',
+                color: '#FF6634',
               },
             },
           },
